2165edd25d1bec7d9adae26a8256e170d43ec4a3
CM <38393728+ora0600@users.noreply.github.com>
2019-09-09 10:10:32 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 resource "aws_instance" "cp53" {
   ami           = "${data.aws_ami.ami.id}"
-  instance_type = "t2.large"
-  key_name      = "your-key"
-  vpc_security_group_ids = ["your security group id"]
-  subnet_id = "your subnet id"
-  private_ip = "172.31.30.30"
+  count         = var.instance_count
+  instance_type = var.instance_type_resource
+  key_name      = var.ssh_key_name
+  vpc_security_group_ids = [var.vpc_securitygroup_id]
+  subnet_id = var.vpc_subnet_id
+  private_ip = var.vpc_private_ip
   
   user_data = data.template_file.confluent_instance.rendered
 
