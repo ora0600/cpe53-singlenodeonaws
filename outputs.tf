192e848880f92878cfc94fe5eef2ade6f0159b0c
CM <38393728+ora0600@users.noreply.github.com>
2019-09-10 13:07:38 +0200

    Add files via upload

---
+++ outputs.tf
@@ -0,0 +1,10 @@
+###########################################
+################# Outputs #################
+###########################################
+
+output "SSH" {
+  value = tonumber(var.instance_count) >= 1 ? "SSH  Access: ssh -i hackathon-temp-key.pem ec2-user@${join(",",formatlist("%s", aws_instance.cp53.*.public_ip),)} " : "Confluent Cloud Platform on AWS is disabled" 
+}
+output "SSH_Tunnel" {
+  value = tonumber(var.instance_count) >= 1 ? "With Tunnel: ssh -i hackathon-temp-key.pem -N -L 9022: ec2-user@${join(",", formatlist("%s", aws_instance.cp53.*.private_dns),)} ec2-user@${join(",", formatlist("%s", aws_instance.cp53.*.public_ip),)}" : "Confluent Cloud Platform on AWS is disabled"
+}  
