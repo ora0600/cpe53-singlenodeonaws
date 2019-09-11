172d13dc66435657ad12fa8c465ff1d39dee13b8
CM <38393728+ora0600@users.noreply.github.com>
2019-09-11 13:03:16 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -6,5 +6,5 @@ output "SSH" {
   value = tonumber(var.instance_count) >= 1 ? "SSH  Access: ssh -i hackathon-temp-key.pem ec2-user@${join(",",formatlist("%s", aws_instance.cp53.*.public_ip),)} " : "Confluent Cloud Platform on AWS is disabled" 
 }
 output "SSH_Tunnel" {
-  value = tonumber(var.instance_count) >= 1 ? "With Tunnel: ssh -i hackathon-temp-key.pem -N -L 9022:@${join(",", formatlist("%s", aws_instance.cp53.*.private_dns),)} ec2-user@${join(",", formatlist("%s", aws_instance.cp53.*.public_ip),)}" : "Confluent Cloud Platform on AWS is disabled"
+  value = tonumber(var.instance_count) >= 1 ? "With Tunnel: ssh -i hackathon-temp-key.pem -N -L 9022:@${join(",", formatlist("%s", aws_instance.cp53.*.private_dns),)}:9021 ec2-user@${join(",", formatlist("%s", aws_instance.cp53.*.public_ip),)}" : "Confluent Cloud Platform on AWS is disabled"
 }  
